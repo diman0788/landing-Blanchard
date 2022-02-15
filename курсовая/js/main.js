177231d1fb6d4528497cbec3dc117eed7c6c813e
Дмитрий <99649860+diman0788@users.noreply.github.com>
2022-02-15 19:55:56 +0400

    курсовая

---
+++ курсовая/js/main.js
@@ -0,0 +1,415 @@
+// плавный скрол
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
+
+// header
+
+let intervelId;
+
+document.querySelectorAll('.header-bottom-lift__btn').forEach(e => {
+  e.addEventListener('click', e => {
+    const menu = e.currentTarget.dataset.path_header;
+    document.querySelectorAll('.header-bottom-left__dropdaun').forEach(e => {
+      if (!document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+        e.classList.remove('dropdaun-is-active');
+        e.classList.remove('open');
+        document.querySelector(`[data-target=${menu}]`).classList.add('dropdaun-is-active')
+        intervelId = setTimeout(() => {
+          document.querySelector(`[data-target=${menu}]`).classList.add('open');
+        }, 0)
+      }
+
+      if (document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+        document.querySelector(`[data-target=${menu}]`).classList.remove('dropdaun-is-active')
+        intervelId = setTimeout(() => {
+          document.querySelector(`[data-target=${menu}]`).classList.remove('open');
+        }, 0)
+      }
+
+      window.onclick = e => {
+        if (e.target == document.querySelector(`[data-target=${menu}]`) || e.target == document.querySelector(`[data-path_header=${menu}]`)) {
+          return;
+        } else {
+          document.querySelector(`[data-target=${menu}]`).classList.remove('dropdaun-is-active');
+          document.querySelector(`[data-target=${menu}]`).classList.remove('open');
+        }
+      }
+    })
+  })
+})
+
+// gallery
+
+const galSwiper = new Swiper('.gallery-right-swiper-container', {
+    pagination: {
+        el: '.gallery-right-swiper-button__pagination',
+        type: 'fraction',
+        simulateTouch: true,
+        touchRatio: 1,
+        touchAngle: 45,
+        grabCursor: true,
+    },
+    navigation: {
+        nextEl: '.gallery-right-swiper-button__next',
+        prevEl: '.gallery-right-swiper-button__prev',
+    },
+    slidesPerView: 1,
+    watchOverflow: true,
+    slidesPerGroup: 1,
+    centeredSlides: false,
+    initialSlide: 1,
+    slidesPerColumn: 2,
+
+    breakpoints: {
+        300: {
+            slidesPerView: 1,
+            autoHeight: false,
+            watchOverflow: true,
+            slidesPerGroup: 1,
+            centeredSlides: true,
+            initialSlide: 0,
+            slidesPerColumn: 1,
+        },
+        500: {
+            slidesPerView: 2,
+            watchOverflow: true,
+            spaceBetween: 34,
+            slidesPerGroup: 2,
+            initialSlide: 0,
+            slidesPerColumn: 1,
+        },
+        1171: {
+            slidesPerView: 3,
+            watchOverflow: true,
+            spaceBetween: 50,
+            slidesPerGroup: 3,
+            centeredSlides: false,
+            initialSlide: 0,
+            slidesPerColumn: 1,
+        },
+    },
+});
+
+// select
+
+const mySelekt = () => {
+    const element = document.querySelector('.section-left__select');
+    const choices = new Choices(element, {
+        position: 'bottom',
+        searchEnabled: false,
+        itemSelectText: ' ',
+    });
+};
+
+mySelekt();
+
+class Modal {
+    constructor(options) {
+        let defaultOptions = {
+            isOpen: () => {},
+            isClose: () => {},
+        };
+        this.options = Object.assign(defaultOptions, options);
+        this.modal = document.querySelector('.modal');
+        this.speed = false;
+        this.animation = false;
+        this.isOpen = false;
+        this.modalContainer = false;
+        this.previousActiveElement = false;
+        this.fixBlocks = document.querySelectorAll('.fix-block');
+        this.focusElements = [
+            'a[href]',
+            'input',
+            'button',
+            'select',
+            'textarea',
+            '[tabindex]',
+        ];
+        this.events();
+    }
+
+    events() {
+        if (this.modal) {
+            document.addEventListener(
+                'click',
+                function(e) {
+                    const clickedElement = e.target.closest('[data-path]');
+                    if (clickedElement) {
+                        let target = clickedElement.dataset.path;
+                        let animation = clickedElement.dataset.animation;
+                        let speed = clickedElement.dataset.speed;
+                        this.animation = animation ? animation : 'fade';
+                        this.speed = speed ? parseInt(speed) : 300;
+                        this.modalContainer = document.querySelector(
+                            `[data-target="${target}"]`
+                        );
+                        this.open();
+                        return;
+                    }
+
+                    if (e.target.closest('.gallery-modal-card-close')) {
+                        this.close();
+                        return;
+                    }
+                }.bind(this)
+            );
+
+            window.addEventListener(
+                'keydown',
+                function(e) {
+                    if (e.keyCode == 27) {
+                        if (this.isOpen) {
+                            this.close();
+                        }
+                    }
+
+                    if (e.keyCode == 9 && this.isOpen) {
+                        this.focusCatch(e);
+                        return;
+                    }
+                }.bind(this)
+            );
+
+            this.modal.addEventListener(
+                'click',
+                function(e) {
+                    if (!e.target.classList.contains('gallery-modal-card') &&
+                        !e.target.closest('.gallery-modal-card') &&
+                        this.isOpen
+                    ) {
+                        this.close();
+                    }
+                }.bind(this)
+            );
+        }
+    }
+
+    open() {
+        this.previousActiveElement = document.activeElement;
+
+        this.modal.style.setProperty('--transition-time', `${this.speed / 1000}s`);
+        this.modal.classList.add('is-open');
+        this.disableScroll();
+
+        this.modalContainer.classList.add('modal-open');
+        this.modalContainer.classList.add(this.animation);
+
+        setTimeout(() => {
+            this.options.isOpen(this);
+            this.modalContainer.classList.add('animate-open');
+            this.isOpen = true;
+            this.focusTrap();
+        }, this.speed);
+    }
+
+    close() {
+        if (this.modalContainer) {
+            this.modalContainer.classList.remove('animate-open');
+            this.modalContainer.classList.remove(this.animation);
+            this.modal.classList.remove('is-open');
+            this.modalContainer.classList.remove('modal-open');
+
+            this.enableScroll();
+            this.options.isClose(this);
+            this.isOpen = false;
+            this.focusTrap();
+        }
+    }
+
+    focusCatch(e) {
+        const focusable = this.modalContainer.querySelectorAll(this.focusElements);
+        const focusArray = Array.prototype.slice.call(focusable);
+        const focusedIndex = focusArray.indexOf(document.activeElement);
+
+        if (e.shiftKey && focusedIndex === 0) {
+            focusArray[focusArray.length - 1].focus();
+            e.preventDefault();
+        }
+
+        if (!e.shiftKey && focusedIndex === focusArray.length - 1) {
+            focusArray[0].focus();
+            e.preventDefault();
+        }
+    }
+
+    focusTrap() {
+        const focusable = this.modalContainer.querySelectorAll(this.focusElements);
+        if (this.isOpen) {
+            focusable[0].focus();
+        } else {
+            this.previousActiveElement.focus();
+        }
+    }
+
+    disableScroll() {
+        let pagePosition = window.scrollY;
+        this.lockPadding();
+        document.body.classList.add('disable-scroll');
+        document.body.dataset.position = pagePosition;
+        document.body.style.top = -pagePosition + 'px';
+    }
+
+    enableScroll() {
+        let pagePosition = parseInt(document.body.dataset.position, 10);
+        this.unlockPadding();
+        document.body.style.top = 'auto';
+        document.body.classList.remove('disable-scroll');
+        window.scroll({ top: pagePosition, left: 0 });
+        document.body.removeAttribute('data-position');
+    }
+
+    lockPadding() {
+        let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
+        this.fixBlocks.forEach((el) => {
+            el.style.paddingRight = paddingOffset;
+        });
+        document.body.style.paddingRight = paddingOffset;
+    }
+
+    unlockPadding() {
+        this.fixBlocks.forEach((el) => {
+            el.style.paddingRight = '0px';
+        });
+        document.body.style.paddingRight = '0px';
+    }
+}
+
+const modal = new Modal();
+
+// accordion
+
+$( function() {
+  $( "#accordion" ).accordion({
+        heightStyle: 'content'
+  });
+});
+
+// events
+
+const eventSwiper = new Swiper('.section-events-swiper-container', {
+  // pagination: {
+  //     el: '.events-list-swiper-button__pagination',
+  //     type: 'bullets',
+  //     simulateTouch: true,
+  //     touchRatio: 1,
+  //     touchAngle: 45,
+  //     grabCursor: true,
+  // },
+  navigation: {
+      nextEl: '.events-list-swiper-button__next',
+      prevEl: '.events-list-swiper-button__prev',
+  },
+  slidesPerView: 1,
+  watchOverflow: true,
+  slidesPerGroup: 1,
+  centeredSlides: false,
+  initialSlide: 1,
+  slidesPerColumn: 1,
+
+  breakpoints: {
+      300: {
+          slidesPerView: 1,
+          autoHeight: false,
+          watchOverflow: true,
+          slidesPerGroup: 1,
+          centeredSlides: true,
+          initialSlide: 12,
+          slidesPerColumn: 1,
+      },
+      500: {
+          slidesPerView: 2,
+          watchOverflow: true,
+          spaceBetween: 34,
+          slidesPerGroup: 2,
+          initialSlide: 7,
+          slidesPerColumn: 2,
+          pagination: {
+            el: '.events-list-swiper-button__pagination',
+            type: 'bullets',
+          },
+      },
+      1171: {
+          slidesPerView: 3,
+          watchOverflow: true,
+          spaceBetween: 50,
+          slidesPerGroup: 3,
+          centeredSlides: false,
+          initialSlide: 2,
+          slidesPerColumn: 1,
+      },
+  },
+});
+
+
+// projects
+
+const projectsSwiper = new Swiper('.projects-bottom__partners', {
+
+  navigation: {
+      nextEl: '.projects-bottom__next',
+      prevEl: '.projects-bottom__prev',
+  },
+  slidesPerView: 1,
+  watchOverflow: true,
+  slidesPerGroup: 1,
+  centeredSlides: false,
+  initialSlide: 1,
+  slidesPerColumn: 1,
+
+  breakpoints: {
+      300: {
+          slidesPerView: 1,
+          autoHeight: false,
+          watchOverflow: true,
+          slidesPerGroup: 1,
+          centeredSlides: true,
+          initialSlide: 0,
+          slidesPerColumn: 1,
+      },
+      500: {
+          slidesPerView: 2,
+          watchOverflow: true,
+          spaceBetween: 34,
+          slidesPerGroup: 1,
+          initialSlide: 0,
+          slidesPerColumn: 1,
+      },
+      1171: {
+          slidesPerView: 3,
+          watchOverflow: true,
+          spaceBetween: 50,
+          slidesPerGroup: 1,
+          centeredSlides: false,
+          initialSlide: 0,
+          slidesPerColumn: 1,
+      },
+  },
+});
+
+// justValidate
+
+// new JustValidate('.contacts-form-wrapper', {
+//     rules: {
+//         name: {
+//             required: true,
+//             minLength: 2,
+//             maxLength: 30,
+//         },
+//         phone: {
+//             required: true,
+//             phone: true,
+//         },
+//     },
+// });
+
+
---
+++ курсовая/js/maps.js
@@ -0,0 +1,35 @@
+// maps
+
+ymaps.ready(function() {
+  var myMap = new ymaps.Map('map', {
+          center: [55.75846806898367,37.60108849999989],
+          zoom: 15,
+          controls: [],
+      }),
+      myPlacemark = new ymaps.Placemark(
+        myMap.getCenter(),
+        {
+          hintContent: '',
+        },
+        {
+          iconLayout: 'default#imageWithContent',
+          iconImageHref: '../img/contacts/contacts-metka.svg',
+          iconImageSize: [20, 20],
+      }
+      );
+  var zoomControl = new ymaps.control.ZoomControl({
+      options: {
+          position: { right: 0, top: 100 },
+          size: 'small',
+      },
+  });
+  var geolocationControl = new ymaps.control.GeolocationControl({
+      options: {
+          position: { right: 0, top: 170 },
+      },
+  });
+  myMap.controls.add(zoomControl);
+  myMap.controls.add(geolocationControl);
+  myMap.geoObjects.add(myPlacemark);
+  myMap.behaviors.disable('scrollZoom');
+});
